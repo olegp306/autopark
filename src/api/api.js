f1f00a2021add477e3659e4132e3d8f6fff13316
oleg <olegp306@gmail.com>
2019-07-14 11:23:26 +0300

    add api for get data structure

---
+++ src/api/api.js
@@ -0,0 +1,30 @@
+const fetchAllBuses = () => {
+  //
+};
+
+const addBus = bus => {
+  //
+};
+const updateBus = bus => {
+  //
+};
+
+const fetchAllDrivers = () => {
+  //
+};
+
+const addDriver = driver => {
+  //
+};
+const updateDrivers = driver => {
+  //
+};
+
+export default {
+  fetchAllBuses,
+  addBus,
+  updateBus,
+  fetchAllDrivers,
+  addDriver,
+  updateDrivers
+};
