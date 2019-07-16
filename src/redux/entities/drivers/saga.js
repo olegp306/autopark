f4a42e7ee2787b3fe32b9ea615d3aca0b3a03647
oleg <olegp306@gmail.com>
2019-07-16 13:33:31 +0300

    fix fetch data from async storage

--- src/redux/entities/drivers/saga.js
+++ src/redux/entities/drivers/saga.js
@@ -3,11 +3,12 @@ import { call, put } from "redux-saga/effects";
 import { isFetching, fetched, fetchFailed, fetch } from "./actions";
 import api from "../../../api/api.js";
 
+
 function* fetchDriversSaga() {
   yield put(isFetching());
   try {
-    yield call(api.fetchAllDrivers);
-    yield put(fetched(response.data));
+    const response=yield call(api.fetchAllDrivers);  
+    yield put(fetched(response));
   } catch (error) {
     yield put(fetchFailed(error));
   }
