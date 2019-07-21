30d53adfb9fb64af532b1799bc0162557f036ba7
oleg <olegp306@gmail.com>
2019-07-21 13:00:23 +0300

    add suggestions reducer

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -3,12 +3,14 @@ import drivers from "./entities/drivers/reducers";
 import driver from "./entities/driver/reducers";
 import buses from "./entities/buses/reducers";
 import bus from "./entities/bus/reducers";
+import suggestions from "./dadata/reducers";
 
 const root = combineReducers({
   drivers,
   driver,
   buses,
-  bus
+  bus,
+  suggestions
 });
 
 export default root;
